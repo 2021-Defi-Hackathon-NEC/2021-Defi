7994ed06032d32cc4934050f70faf641b1cac2a1
jimmimmim <likecharry@gmail.com>
2021-07-17 03:00:05 +0900

    goodnight

--- wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/MainActivity.java
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.wallet_bottom_nav_2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,11 +30,30 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_exchange, R.id.navigation_wallet,
+                R.id.navigation_notifications, R.id.navigation_dashboard)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
+    
+    // 액션바 버튼
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+//        return true;
+//    }
+//
+//    @SuppressLint("NonConstantResourceId")
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle presses on the action bar items
+//        if (item.getItemId() == R.id.action_btn1) {
+//            playBtn();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
