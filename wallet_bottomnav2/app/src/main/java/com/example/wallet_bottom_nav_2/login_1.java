df206d62de71c7388cb0b33aae0534760636c2c3
geniee44 <68368633+geniee44@users.noreply.github.com>
2021-07-20 21:13:50 +0900

    login_2 design

--- wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/login_1.java
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/login_1.java
@@ -2,7 +2,11 @@ package com.example.wallet_bottom_nav_2;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_1 extends AppCompatActivity {
 
@@ -13,5 +17,15 @@ public class login_1 extends AppCompatActivity {
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.hide();
+
+        Button imageButton = (Button) findViewById(R.id.button4);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), login_2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
