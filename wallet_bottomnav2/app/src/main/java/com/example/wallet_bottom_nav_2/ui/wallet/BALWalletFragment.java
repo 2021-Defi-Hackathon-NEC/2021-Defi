3f32e3adfbd3366aebd527f6611062e4380f620a
jimmimmim <likecharry@gmail.com>
2021-07-23 18:54:31 +0900

    wallet updated

--- wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/ui/wallet/BALWalletFragment.java
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/ui/wallet/BALWalletFragment.java
@@ -1,10 +1,14 @@
 package com.example.wallet_bottom_nav_2.ui.wallet;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -33,6 +37,19 @@ public class BALWalletFragment extends AppCompatActivity {
         }
     }
 
+    public void clickBtn_clipboard_copy(View view){
+        switch(view.getId()){
+            case R.id.textView12:
+                TextView title1 = (TextView) findViewById(R.id.textView12);
+                // Gets a handle to the clipboard service.
+                ClipboardManager clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+                // Creates a new text clip to put on the clipboard
+                ClipData clip = ClipData.newPlainText("simple text", title1.getText().toString());
+                Toast.makeText(getApplicationContext(), "Copied!", Toast.LENGTH_SHORT).show();
+                // Set the clipboard's primary clip.
+                clipboard.setPrimaryClip(clip);
+        }
+    }
 }
 
 
