6ad9a7ca71c2a62584e24b427ff34acfae69a131
geniee44 <68368633+geniee44@users.noreply.github.com>
2021-07-24 01:42:17 +0900

    login plz

--- wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/login3_ethereum.java
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/login3_ethereum.java
@@ -3,12 +3,14 @@ package com.example.wallet_bottom_nav_2;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,12 +18,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import jnr.ffi.annotations.In;
+
+import static android.R.layout.simple_spinner_item;
+
 public class login3_ethereum extends AppCompatActivity {
     private EditText Mnemonic = (EditText) findViewById(R.id.Mnemonic);
+    private Button nextButton = (Button) findViewById(R.id.nextButton);
+    private EditText privatekey = (EditText) findViewById(R.id.editPrivate);
     private Spinner spinner_coins;
     String[] spinnerNames;
     int[] spinnerImages;
     int selected_coin_idx = 0;
+    private boolean eth = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,30 +68,77 @@ public class login3_ethereum extends AppCompatActivity {
         Button addButton = (Button) findViewById(R.id.addButton) ;
         Mnemonic.setTypeface(Typeface.DEFAULT);
 
+        //Button invisible
         addButton.setVisibility(View.GONE);
         addButton.setClickable(false);
+        Mnemonic.setVisibility(View.GONE);
+        Mnemonic.setClickable(false);
+        nextButton.setVisibility(View.GONE);
+        nextButton.setClickable(false);
 
+        //Ethereum Mnemonic
+        ArrayAdapter<String> coinAdapter = new ArrayAdapter<>(this, simple_spinner_item, spinnerNames);
+        coinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Mnemonic.addTextChangedListener(new TextWatcher() {
+        spinner_coins.setAdapter(coinAdapter);
+
+        spinner_coins.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (editable.length() > 3) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(spinnerNames[position].equals("Ethereum")){
+                    Mnemonic.setClickable(true);
+                    Mnemonic.setVisibility(View.VISIBLE);
                     addButton.setVisibility(View.VISIBLE);
                     addButton.setClickable(true);
-                } else {
-                    addButton.setClickable(false);
+                    nextButton.setVisibility(View.VISIBLE);
+                    eth = true;
+
+                    Mnemonic.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable editable) {
+                            if (editable.length() > 3) {
+                                nextButton.setClickable(true);
+                            } else {
+                                nextButton.setClickable(false);
+                            }
+                        }
+                    });
+                }
+                else{
+                    addButton.setClickable(true);
+                    addButton.setVisibility(View.VISIBLE);
                 }
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //한희님 여기예요
+        //여기 고칠거 많아요
+        //주목해주세요
+        nextButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if ((eth&&(Mnemonic==privatekey))||!eth&&(1==1)) {
+                    Intent intent = new Intent(login3_ethereum.this, login_popup_great.class);
+                }
+                else{
+                    Intent intent = new Intent(login3_ethereum.this, login_popup_error.class);
+                }
+            }
+
         });
 
     }
--- wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/login_popup_great.java
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/login_popup_great.java
@@ -3,12 +3,23 @@ package com.example.wallet_bottom_nav_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
 
 public class login_popup_great extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
         setContentView(R.layout.activity_login_popup_great);
+
+
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        if( event.getAction() == MotionEvent.ACTION_OUTSIDE ) {
+            return false;
+        }
+        return true;
     }
 }
