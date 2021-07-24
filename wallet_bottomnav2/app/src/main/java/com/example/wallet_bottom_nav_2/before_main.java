3be374c662fb07ac6e295eaa204f4575f53e8641
hanhe <hanhee130@gmail.com>
2021-07-25 04:17:13 +0900

    intro

--- wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/before_main.java
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/before_main.java
@@ -3,32 +3,34 @@ package com.example.wallet_bottom_nav_2;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.view.View;
+
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 
 public class before_main extends AppCompatActivity {
+
+    private Context context;
+    private boolean isLoggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_before_main);
+        context = this;
 
         Handler hd = new Handler(Looper.getMainLooper());
         hd.postDelayed(new SplashHandler(), 3000);
 
         // 액션바 숨기기
         ActionBar actionbar = getSupportActionBar();
-        actionbar.hide();
+        if (actionbar != null) { actionbar.hide(); }
 
         // 상태바 숨기기
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -43,19 +45,21 @@ public class before_main extends AppCompatActivity {
             );
         }
 
-        
-    }
+        String user = PreferenceManager.getString(context, "user");
+        if (user.equals("")) { isLoggedIn = false; }
 
-    
+    }
     
     private class SplashHandler implements Runnable {
         public void run(){
-            startActivity(new Intent(getApplication(), MainActivity.class));
+            if (isLoggedIn) {
+                startActivity(new Intent(getApplication(), MainActivity.class));
+            } else {
+//                startActivity(new Intent(getApplication(), login_1.class));
+                startActivity(new Intent(getApplication(), login_1.class));
+            }
             before_main.this.finish();
         }
-
-
-
     }
 
 }
