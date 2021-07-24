64311a56ff355074bf9a237fd7f990044b00f907
Chaerin Kim <decentralized0212@gmail.com>
2021-07-24 22:37:54 +0900

    오류 해결

--- wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/coinmarketcap.java
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/coinmarketcap.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import android.view.View;
 import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class coinmarketcap extends AppCompatActivity {
@@ -25,14 +28,14 @@ public class coinmarketcap extends AppCompatActivity {
     private static final String TAG = "Main";
 
     EditText edit_coinNm;
-    TextView opening_price; //시가
-    TextView high_price; //고가
-    TextView low_price; //저가
-    TextView trade_price; //종가
-    TextView prev_closing_price; //전일 증가
-    TextView acc_trade_price_24h; //24시간 누적 거래대금
-    TextView acc_trade_volume_24h; //24시간 누적 거래량
-    Button btn_search;
+    public TextView opening_price; //시가
+    public TextView high_price; //고가
+    public TextView low_price; //저가
+    public TextView trade_price; //종가
+    public TextView prev_closing_price; //전일 증가
+    public TextView acc_trade_price_24h; //24시간 누적 거래대금
+    public TextView acc_trade_volume_24h; //24시간 누적 거래량
+    public Button btn_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +49,8 @@ public class coinmarketcap extends AppCompatActivity {
         prev_closing_price = findViewById(R.id.prev_closing_price);
         acc_trade_price_24h = findViewById(R.id.acc_trade_price_24h);
         acc_trade_volume_24h = findViewById(R.id.acc_trade_volume_24h);
-        button btn_search = findViewById(R.id.btn_search);
+
+        Button btn_search = findViewById(R.id.btn_search);
         btn_search.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -54,7 +58,7 @@ public class coinmarketcap extends AppCompatActivity {
 
                 String coinNm = edit_coinNm.getText().toString().toUpperCase();
 
-                if(!coinNm.isEnpty()){
+                if(!coinNm.isEmpty()){
                     getTicker(coinNm);
                 }
             }
@@ -71,12 +75,12 @@ public class coinmarketcap extends AppCompatActivity {
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                getTickerData(responce);
+                getTickerData(response);
             }
         },
                 new Response.ErrorListener() {
                     @Override
-                    public void onErrorResponse(VollyError error) {
+                    public void onErrorResponse(VolleyError error) {
                         Log.d(TAG, "onErrorResponse:" + error.getMessage());
                     }
                 }) {
@@ -90,9 +94,9 @@ public class coinmarketcap extends AppCompatActivity {
             JSONArray jsonArray = new JSONArray(data);
 
             for (int i = 0; i< jsonArray.length(); i++) {
-                JSONObject jsonArray = jsonArray.getJSONObject(i);
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-                String sPpening_price = jsonObject.get("opening_price").toString(); //시가
+                String sPening_price = jsonObject.get("opening_price").toString(); //시가
                 String sHigh_price = jsonObject.get("high_price").toString(); //고가
                 String sLow_price = jsonObject.get("low_price").toString(); //저가
                 String sTrade_price = jsonObject.get("trade_price").toString(); //종가
@@ -101,14 +105,20 @@ public class coinmarketcap extends AppCompatActivity {
                 String sAcc_trade_volume_24h = jsonObject.get("acc_trade_volume_24h").toString(); //24시간 누적 거래량
 
                 //나중에 프론트에서 텍스트뷰에서 사용해야 하기 때문에......... 포멧을 예쁘게...
-
-                opening_price.setText(toDoubleFormat(Double.parseDouble(sPpening_price)));
-                high_price.setText(toDoubleFormat(Double.parseDouble(sHigh_price)));
-                low_price.setText(toDoubleFormat(Double.parseDouble(sLow_price)));
-                trade_price.setText(toDoubleFormat(Double.parseDouble(sTrade_price)));
-                prev_closing_price.setText(toDoubleFormat(Double.parseDouble(sPrev_closing_price)));
-                acc_trade_price_24h.setText(toDoubleFormat(Double.parseDouble(sAcc_trade_price_24h)));
-                acc_trade_volume_24h.setText(toDoubleFormat(Double.parseDouble(sAcc_trade_volume_24h)));
+                opening_price.setText((int) Double.parseDouble(sPening_price));
+                high_price.setText((int) Double.parseDouble(sHigh_price));
+                low_price.setText((int) Double.parseDouble(sLow_price));
+                trade_price.setText((int) Double.parseDouble(sTrade_price));
+                prev_closing_price.setText((int) Double.parseDouble(sPrev_closing_price));
+                acc_trade_price_24h.setText((int) Double.parseDouble(sAcc_trade_price_24h));
+                acc_trade_volume_24h.setText((int) Double.parseDouble(sAcc_trade_volume_24h));
+//                opening_price.setText(toDoubleFormat(Double.parseDouble(sPpening_price)));
+//                high_price.setText(toDoubleFormat(Double.parseDouble(sHigh_price)));
+//                low_price.setText(toDoubleFormat(Double.parseDouble(sLow_price)));
+//                trade_price.setText(toDoubleFormat(Double.parseDouble(sTrade_price)));
+//                prev_closing_price.setText(toDoubleFormat(Double.parseDouble(sPrev_closing_price)));
+//                acc_trade_price_24h.setText(toDoubleFormat(Double.parseDouble(sAcc_trade_price_24h)));
+//                acc_trade_volume_24h.setText(toDoubleFormat(Double.parseDouble(sAcc_trade_volume_24h)));
             }
         }catch (JSONException e){
             e.printStackTrace();
