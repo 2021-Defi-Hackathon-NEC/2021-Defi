771fc04afb81e7c65364a613559cc6700395abe7
geniee44 <68368633+geniee44@users.noreply.github.com>
2021-07-24 20:18:47 +0900

    graph ui

---
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/coin_graph.java
@@ -0,0 +1,13 @@
+package com.example.wallet_bottom_nav_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class coin_graph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_graph);
+    }
+}
