048efea078be2cfcf5bd1ca26bac7faac9c67be1
jimmimmim <likecharry@gmail.com>
2021-07-24 23:30:12 +0900

    coinmarket updated

---
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/temp_coinmarketcap.java
@@ -0,0 +1,32 @@
+package com.example.wallet_bottom_nav_2;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class temp_coinmarketcap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.hide();
+
+//        Button imageButton = (Button) findViewById(R.id.button4);
+//        imageButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), login3_ethereum.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+}
