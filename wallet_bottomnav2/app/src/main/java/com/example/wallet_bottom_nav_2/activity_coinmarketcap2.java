e613e41d0db6b32b604f280c8c53fb2c440b2943
jimmimmim <likecharry@gmail.com>
2021-07-24 17:46:33 +0900

    exchange, market updated

---
+++ wallet_bottomnav2/app/src/main/java/com/example/wallet_bottom_nav_2/activity_coinmarketcap2.java
@@ -0,0 +1,14 @@
+package com.example.wallet_bottom_nav_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_coinmarketcap2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coinmarketcap2);
+    }
+}
